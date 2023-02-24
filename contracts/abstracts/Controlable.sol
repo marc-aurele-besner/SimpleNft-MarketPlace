fad253cfb354b45fe6ce1f61dc0d384f645c3197
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-02-24 11:00:46 -0500

    add interface of contract

--- contracts/abstracts/Controlable.sol
+++ contracts/abstracts/Controlable.sol
@@ -2,5 +2,43 @@
 pragma solidity ^0.8.19;
 
 abstract contract Controlable {
-    
+
+    event TransactionFeeChanged();
+
+    function _changeSupportedContract(address contractAddress) internal returns (bool success) {
+
+    }
+    function _changeTransactionFee(uint32 transactionFee) internal returns (bool success) {
+
+    }
+
+    // Treasury
+    function _withdrawTransactionFee() internal returns (bool success) {
+
+    }
+
+    // Moderator
+    function _blacklistToken(address tokenContract, uint256 tokenId) internal returns (bool success) {
+
+    }
+    function _blacklistUser(address userAddress) internal returns (bool success) {
+
+    }
+
+    function transactionFee() public returns (uint32 transactionFee) {
+
+    }
+    function token() public returns (address tokenAddress) {
+
+    }
+
+    function isAdmin(address account) public returns (bool isAdmin) {
+
+    }
+    function isTreasury(address account) public returns (bool isTreasury) {
+
+    }
+    function isModerator(address account) public returns (bool isModerator) {
+
+    }
 }
