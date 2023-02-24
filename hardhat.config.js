4fd1a26fb23a2abea4bab591d2582520111fa8e8
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-02-24 10:26:44 -0500

    add foundry & hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-awesome-cli");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          }
+        }
+      },
+    ],
+  },
+};
