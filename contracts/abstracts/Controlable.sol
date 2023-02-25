8473ee5d5864685cefb769a53541079866ff26d2
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-02-25 21:08:37 +0100

    add function, modifier

--- contracts/abstracts/Controlable.sol
+++ contracts/abstracts/Controlable.sol
@@ -7,11 +7,18 @@ abstract contract Controlable {
     mapping(address => bool) public admin;
     mapping(address => bool) public treasury;
     mapping(address => bool) public moderator;
+    
+    string public transactionFee;
 
-    event TransactionFeeChanged();
+    event TransactionFeeChanged(uint indexed oldFee, uint indexed newFee);
 
     modifier onlyModerator() {
         require(moderator[msg.sender] == true, "Is not a moderator");
+        _;
+    }
+
+    modifier onlyAdmin() {
+        require(admin[msg.sender] == true, "Is not an admin");
         _;
     }
 
@@ -19,14 +26,15 @@ abstract contract Controlable {
 
     }
 
-    function _changeTransactionFee(uint32 transactionFee) internal returns (bool success) {
-
+    function _changeTransactionFee(uint32 _transactionFee) internal onlyAdmin returns (bool success) {
+        transactionFee = _transactionFee;
+        emit TransactionFeeChanged(_transactionFee, transactionFee);
     }
 
     // Treasury
-    function _withdrawTransactionFee() internal returns (bool success) {
-
-    }
+    // function _withdrawTransactionFee() internal onlyAdmin returns (bool success) {
+    //     (success, ) = payable(treasury).call{ value: address.treasury.balance }('');
+    // }
 
     // Moderator
     function _blacklistToken(address tokenContract, uint256 tokenId) internal onlyModerator returns (bool success) {
@@ -37,12 +45,12 @@ abstract contract Controlable {
         blacklistUser[userAddress] = true; 
     }
 
-    function transactionFee() public returns (uint32 transactionFee) {
+    function transactionFee() public returns (uint32 _transactionFee) {
         return transactionFee;
     }
 
     function token() public returns (address tokenAddress) {
-        return tokenAddress;
+        
     }
 
     function isAdmin(address account) public returns (bool isAdmin) {
