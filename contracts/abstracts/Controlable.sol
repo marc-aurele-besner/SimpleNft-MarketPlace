a66dfcdc305fbdc9bec9c300e5232ed452f4a2b4
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-02-25 20:08:31 +0100

    add function, modifier, mapping

--- contracts/abstracts/Controlable.sol
+++ contracts/abstracts/Controlable.sol
@@ -2,12 +2,23 @@
 pragma solidity ^0.8.19;
 
 abstract contract Controlable {
+    mapping(address => mapping(uint => bool)) private blacklistToken;
+    mapping(address => bool) private blacklistUser;
+    mapping(address => bool) public admin;
+    mapping(address => bool) public treasury;
+    mapping(address => bool) public moderator;
 
     event TransactionFeeChanged();
+
+    modifier onlyModerator() {
+        require(moderator[msg.sender] == true, "Is not a moderator");
+        _;
+    }
 
     function _changeSupportedContract(address contractAddress) internal returns (bool success) {
 
     }
+
     function _changeTransactionFee(uint32 transactionFee) internal returns (bool success) {
 
     }
@@ -18,27 +29,31 @@ abstract contract Controlable {
     }
 
     // Moderator
-    function _blacklistToken(address tokenContract, uint256 tokenId) internal returns (bool success) {
-
+    function _blacklistToken(address tokenContract, uint256 tokenId) internal onlyModerator returns (bool success) {
+        blacklistToken[tokenContract][tokenId] = true;
     }
-    function _blacklistUser(address userAddress) internal returns (bool success) {
 
+    function _blacklistUser(address userAddress) internal onlyModerator returns (bool success) {
+        blacklistUser[userAddress] = true; 
     }
 
     function transactionFee() public returns (uint32 transactionFee) {
-
+        return transactionFee;
     }
-    function token() public returns (address tokenAddress) {
 
+    function token() public returns (address tokenAddress) {
+        return tokenAddress;
     }
 
     function isAdmin(address account) public returns (bool isAdmin) {
-
+        return admin[account];
     }
+
     function isTreasury(address account) public returns (bool isTreasury) {
-
+        return treasury[account];
     }
-    function isModerator(address account) public returns (bool isModerator) {
 
+    function isModerator(address account) public returns (bool isModerator) {
+        return moderator[account];
     }
 }
