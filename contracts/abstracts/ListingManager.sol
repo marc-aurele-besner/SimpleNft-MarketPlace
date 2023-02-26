d32f68f9e99ddeb14a79293890f29f693b69a842
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-02-26 12:36:35 +0100

    try to add listing function

--- contracts/abstracts/ListingManager.sol
+++ contracts/abstracts/ListingManager.sol
@@ -2,12 +2,30 @@
 pragma solidity ^0.8.19;
 
 abstract contract ListingManager {
+
+    struct Listing {
+        address tokenContract;
+        uint tokenId;
+        uint salePrice;
+    }
+
+    uint private listingId = 0;
+    mapping(uint => Listing) private _listings;
     
     event Listing();
     event Sale();
 
     function _createListing(address tokenContract, uint256 tokenId, uint256 salePrice) internal returns (uint256 listingId) {
+        Listing memory listing = Listing(
+            tokenContract,
+            tokenId,
+            salePrice
+        );
 
+        listingId++;
+        _listings[listingsId] = listing;
+        
+        emit Listing();
     }
 
     function _buyListing(uint256 listingId) internal returns (bool success) {
