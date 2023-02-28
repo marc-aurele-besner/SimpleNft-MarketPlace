ea49fc53286d0e6cd98fbfc0775406a6a14925b6
mathilde-mi <122474582+mathilde-mi@users.noreply.github.com>
2023-02-28 11:26:33 +0100

    Update Controlable.sol
    Essai de logique pour _changeSupportedContract

--- contracts/abstracts/Controlable.sol
+++ contracts/abstracts/Controlable.sol
@@ -5,9 +5,23 @@ abstract contract Controlable {
 
     event TransactionFeeChanged();
 
+    mapping(address => bool) supportedContracts;
     function _changeSupportedContract(address contractAddress) internal returns (bool success) {
+        require(msg.sender == owner, "Vous n'avez pas les autorisations nécessaires.");
 
+        if (supportedContracts[contractAddress]) {
+        // Le contrat est déjà pris en charge, donc on le supprime
+            supportedContracts[contractAddress] = false;
+            emit SupportedContractRemoved(contractAddress);
+            return true;
+        } else {
+            // Le contrat n'est pas pris en charge, donc on l'ajoute
+            supportedContracts[contractAddress] = true;
+            emit SupportedContractAdded(contractAddress);
+            return true;
+        }
     }
+    
     function _changeTransactionFee(uint32 transactionFee) internal returns (bool success) {
 
     }
