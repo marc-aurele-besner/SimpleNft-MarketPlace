ea8f2f8dd7feffb4d54f3599d7bdc9ccc60d6f24
mathilde-mi <122474582+mathilde-mi@users.noreply.github.com>
2023-03-02 11:31:51 +0100

    deploy the contract and first tests scenarios

--- test/Test-Mathilde.js
+++ test/Test-Mathilde.js
@@ -5,3 +5,30 @@ const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 // Créer des sénarios pour chaque fonction du fichier Controlable.sol, ListingManager.sol, 
 // ValidateSignature.sol, SimpleNftMarketplace.sol
 
+describe('SimpleNftMarketplace', function () {
+    let Contract;
+    let owner, seller, moderator;
+    let listingId;
+    
+    beforeEach(async function () {
+      [owner, seller, moderator] = await ethers.getSigners();
+      const Contract = await ethers.getContractFactory('SimpleNftMarketplace');
+      contract = await Contract.deploy();
+      await contract.deployed();
+    });
+  
+    describe('onlyListingOwnerOrModerator', function () {
+      it('should allow the seller to call the function', async function () {
+
+      });
+  
+      it('should allow a moderator to call the function', async function () {
+        
+      });
+  
+      it('should not allow someone who is not the seller or moderator to call the function', async function () {
+       
+      });
+    });
+  });
+  
