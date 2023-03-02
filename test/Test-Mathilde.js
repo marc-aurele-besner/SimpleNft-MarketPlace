71af7593d4bab3abad158b4d09e8eb157cb594f0
mathilde-mi <122474582+mathilde-mi@users.noreply.github.com>
2023-03-02 10:45:18 +0100

    Création fichier test

---
+++ test/Test-Mathilde.js
@@ -0,0 +1,7 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+
+// Créer des sénarios pour chaque fonction du fichier Controlable.sol, ListingManager.sol, 
+// ValidateSignature.sol, SimpleNftMarketplace.sol
+
