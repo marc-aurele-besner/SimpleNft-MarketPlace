f879ee70c23cafb607f965d03a2ca6d108e488fc
mathilde-mi <122474582+mathilde-mi@users.noreply.github.com>
2023-03-03 14:58:39 +0100

    correction name

--- test/Test-Mathilde.js
+++ test/Test-Mathilde.js
@@ -2,9 +2,6 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 
-// Créer des sénarios pour chaque fonction du fichier Controlable.sol, ListingManager.sol, 
-// ValidateSignature.sol, SimpleNftMarketplace.sol
-
 describe('SimpleNftMarketplace', function () {
     let Contract;
     let owner, seller, moderator;
@@ -16,25 +13,11 @@ describe('SimpleNftMarketplace', function () {
       contract = await Contract.deploy();
       await contract.deployed();
     });
-  
-    describe('onlyListingOwnerOrModerator', function () {
-      it('should allow the seller to call the function', async function () {
-// logique a écrire 
-      });
-  
-      it('should allow a moderator to call the function', async function () {
-// logique a écrire 
-      });
-  
-      it('should not allow someone who is not the seller or moderator to call the function', async function () {
-// logique a écrire 
-      });
-    });
 
     describe('name', function () {
         it("Should return the correct NAME", async function () {
             const expectedName = "SimpleNft-MarketPlace";
-            const actualName = await contract.NAME;
+            const actualName = await contract.NAME();
             expect(actualName).to.equal(expectedName);
     });
   });
