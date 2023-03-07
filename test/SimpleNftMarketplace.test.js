a28f1343ddd77763bf73ca304bf9d5b47c101eb5
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-03-07 10:26:26 -0500

    add signature test

--- test/SimpleNftMarketplace.test.js
+++ test/SimpleNftMarketplace.test.js
@@ -5,6 +5,8 @@ const {
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
+const Helper = require('./shared');
+
 describe("SimpleNftMarketplace", function () {
   async function deployOneYearLockFixture() {
     // Contracts are deployed using the first signer/account by default
@@ -36,6 +38,31 @@ describe("SimpleNftMarketplace", function () {
       expect(details.buyer).to.be.equal('0x0000000000000000000000000000000000000000');
       expect(details.listingTimestamp).to.be.equal(ethers.BigNumber.from(0));
       expect(details.buyTimestamp).to.be.equal(ethers.BigNumber.from(0));
+    });
+  });
+
+  describe('SimpleNftMarketplace', function () {
+    before(async function () {
+      [provider, owner, user1, user2, user3] = await Helper.setupProviderAndAccount();
+    });
+  
+    beforeEach(async function () {
+      contract = await Helper.setupContract([user1.address, user2.address]);
+    });
+
+    it("Should return default value when calling getListingDetail", async function () {
+      const { simpleNftMarketplace, owner, otherAccount } = await loadFixture(deployOneYearLockFixture);
+
+      const MockERC20 = await ethers.getContractFactory('MockERC20')
+      mockERC20 = await MockERC20.deploy()
+      await mockERC20.deployed()
+
+      const signature = await Helper.signatures.signCreateListing(contract.address, owner, mockERC20.address, 1, 100, owner.address);
+      // await contract.createListing(mockERC20.address, 1, 100, owner.address, signature.v, signature.r, signature.s);
+
+      //  await contract['createListing(address,uint256,uint256)']();
+      // await contract['createListing(address,uint256,uint256,address,uint8,bytes32,bytes32)'](mockERC20.address, 1, 100, owner.address, signature.v, signature.r, signature.s);
+      // console.log('signature', signature);
     });
   });
 });
