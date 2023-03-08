781e2655811782b506ea9c667b538a9c2a7d106c
benwa06 <benwa06@hotmail.fr>
2023-03-08 13:18:00 +0100

    Listing price test update

--- test/BenoitSimpleNftMarketplace.test.js
+++ test/BenoitSimpleNftMarketplace.test.js
@@ -1,88 +1,59 @@
+const { time, loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { expect } = require('chai');
+const Helper = require('./shared');
 
+describe('Test-Benoit', function () {
+    before(async function () {
+        [provider, owner, user1, user2, user3] = await
+    Helper.setupProviderAndAccount();
+    });
+
+    beforeEach (async function () {
+        contract = await Helper.setupContract()
+    })
+    it.only("does it return the correct listing price (should be)", async function () {
+        await Helper.deployERC721();
+        await Helper.mintERC721(user1.address, 1);
+        await mockERC721.connect(user1).approve(contract.address, 1);
+        await contract.connect(user1)['createListing(address,uint256,uint256)'](mockERC721.address, 1, 100);
+        expect(await contract.listingPrice(1)).to.be.equal(100)
+    });
+})  
+
+//#37 read function tests
+
+it("does it return true is the listing is active (should be)", async () => {
+
+});
+
+it("does it return true is the listing is active (should be)", async () => {
+
+});
+
+it("does it return true if a user is blacklisted (should be)", async () => {
+
+});
+
+it("does it return true if a token is blacklisted (should be)", async () => {
+
+});
+
+it("does it return true if a contract is supported (should be)", async () => {
+
+});
+
+it("does it return the correct listing fee (should be)", async () => {
+
+});
 // buy / sell 
-describe("SimpleNftMarketplace - Benoit", function () {
-
     it("Does user can buy NFT (trade eth for token)(should be)", async function () {
     
     });
-
     it("Does user can sell NFT (trade eth for token)(should be)", async function () {
     
     });
-
     // WithDrawEth/ Send
-
     it("Does user can withDrawEth from the marketplace (should be)", async function () {
-
     });
-
     it("Does user can sendEth to the marketplace (should  be)", async function () {
-
     });
-
-
-    //blacklist
-
-    it("can a blacklisted user still withdrawEth (should not)", async function () {
-
-    });
-
-    it("can a blacklisted user still sell NFT (should not)", async function () {
-
-    });
-
-    it("can a blacklisted user still buy NFT (should not)", async function () {
-
-    });
-
-    it("can a blacklisted user still sendEth (should not)", async function () {
-
-    });
-
-    it("can a blakclisted user still create a collection (should not)", async function () {
-
-    });
-    //admin
-
-    it("is the Admin able to withdrawAll Eth sent to the marketplace (should be)", async function () {
-
-    });
-
-    it("is the Admin able to add new moderator (should be)", async function () {
-
-    });
-
-    it("is the Admin able to remove moderator (should be)", async function () {
-
-    });
-
-    it("is the ownerShip changeable", async function () {
-
-    });
-
-    it("can owner change transaction fees (should be", async function () {
-
-    });
-
-    // admin / moderator
-
-    it("is the moderator able to change ownerShip (should not)", async function () {
-
-    });
-
-    it("is the moderator able to withdrawAll eth(should not)", async function () {
-
-    });
-
-
-
-    // admin / user
-
-    it("is the user1 able to change ownerShip (should not)", async function () {
-
-    });
-
-    it("is the user2 able to withdrawAll eth(should not)", async function () {
-
-    });
-});
