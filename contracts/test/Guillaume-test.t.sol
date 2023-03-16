791be51c22dbcb2ac03ccb9d7a1389170f3a53b3
Guillaume-web <cryptomass2022@gmail.com>
2023-03-16 17:55:13 +0100

    Update Guillaume-test.t.sol

--- contracts/test/Guillaume-test.t.sol
+++ contracts/test/Guillaume-test.t.sol
@@ -102,21 +102,16 @@ contract SimpleNftMarketplace_test_guillaume_test is Helper {
 
   function test_SimpleNftMarketplace_basic_blacklistUser_moderator() public {
     verify_revertCall(RevertStatus.CallerNotModerator);
-    helper_blacklistUser(address(2), address(1), true);
     helper_blacklist_user(address(2), address(1), true, RevertStatus.CallerNotModerator);
     assertTrue(!marketplace.isBlacklistedUser(address(1)));
   }
   
   function test_SimpleNftMarketplace_basic_blacklistToken_admin() public {
-    helper_blacklistToken(ADMIN, address(1), 0, true);
     helper_blacklist_token(ADMIN, address(1), 0, true);
     assertTrue(marketplace.isBlacklistedToken(address(1), 0));
   }
 
   function test_SimpleNftMarketplace_basic_blacklistToken_moderator() public {
-    verify_revertCall(RevertStatus.CallerNotModerator);
-    helper_blacklistToken(address(2), address(1), 0, true);
-
     helper_blacklist_token(address(2), address(1), 0, true, RevertStatus.CallerNotModerator);
     assertTrue(!marketplace.isBlacklistedToken(address(1), 0));
   }
