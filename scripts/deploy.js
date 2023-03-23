3e33dbca20b4b0a530a51dd64ad850a954409093
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-03-23 09:29:34 -0400

    fix workflows

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,19 +4,23 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+const hre = require('hardhat');
 
 async function main() {
-  const SimpleNftMarketplace = await ethers.getContractFactory("SimpleNftMarketplace");
+  const SimpleNftMarketplace = await ethers.getContractFactory('SimpleNftMarketplace');
   const simpleNftMarketplace = await SimpleNftMarketplace.deploy();
 
   console.log('tx hash', simpleNftMarketplace.deployTransaction.hash);
 
   await simpleNftMarketplace.deployed();
 
-  console.log(
-    `SimpleNftMarketplace deployed at ${simpleNftMarketplace.address}`
-  );
+  const tx = await simpleNftMarketplace.initialize();
+
+  await tx.wait();
+
+  await simpleNftMarketplace.setFee(100);
+
+  console.log(`SimpleNftMarketplace deployed at ${simpleNftMarketplace.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
